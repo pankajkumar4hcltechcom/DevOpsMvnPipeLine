d4190a7f61cdaaf8fe110242dde908e437458b81
pankajkumar4hcltechcom <pankaj.kumar4@hcltech.com>
2024-07-09 17:23:08 +0530

    Create DevopsTestMvnApplicationTests.java

---
+++ src/test/java/com/devopstest/mv/DevopsTestMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopstest.mv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTestMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
