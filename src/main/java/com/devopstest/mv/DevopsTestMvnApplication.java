25386160af99ae98670e724ca1865a363e98ecc9
pankajkumar4hcltechcom <pankaj.kumar4@hcltech.com>
2024-07-09 17:20:31 +0530

    Create DevopsTestMvnApplication.java

---
+++ src/main/java/com/devopstest/mv/DevopsTestMvnApplication.java
@@ -0,0 +1,13 @@
+package com.devopstest.mv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTestMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTestMvnApplication.class, args);
+	}
+
+}
